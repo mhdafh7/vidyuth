a3de6cb7cffb09fadbe21d137efb74179690618e
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-23 22:40:24 +0530

    feat: Navbar basics

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link'
 import styles from './Navbar.module.scss';
 const Navbar = () => {
-    return <div className={styles.container}>Navbar</div>;
+    return (
+        <div className={styles.container}>
+            <Link href="/">
+                <Image
+                    src="/vidyuth-logo.png"
+                    alt="vidyuth logo"
+                    width={32}
+                    height={32}
+                />
+            </Link>
+            <ul className={styles.navItems}>
+                <Link href="/events"><li>Events</li></Link>
+                <Link href="/contact"><li>Contact</li></Link>
+                <Link href="/collaborators"><li>Collaborators</li></Link>
+            </ul>
+            <a href='http://www.rit.ac.in/' target="_blank" rel="noreferrer">
+                <Image
+                    src="/rit-logo.png"
+                    alt="RIT kottayam logo"
+                    width={32}
+                    height={32}
+                />
+            </a>
+        </div>
+    );
 };
 export default Navbar;
