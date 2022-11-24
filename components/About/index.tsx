4824d58bd6e3116d402785065a1a106dd39a562c
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-24 19:26:08 +0530

    feat: Added about section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,8 +1,30 @@
-import styles from './About.module.scss'
+import Image from 'next/image';
+import styles from './About.module.scss';
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
-export default index
+const About = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.aboutLogo}>
+                <Image
+                    src="/vidyuth-logo.png"
+                    alt="Vidyuth logo"
+                    className={styles.img}
+                    // width={400}
+                    // height={400}
+                    fill
+                    // placeholder="blur"
+                />
+            </div>
+            <p>
+                Vidyuth is the annual technical fest of the Electrical and
+                Department, RIT Kottayam with a footfall of more than 20,000 and
+                an outreach to more than 50 colleges. It is a core campaign
+                unifying each EEEian to focus their gaze on progress, invention
+                and technology. It features an impressive line-up of sci-tech
+                events including Lectures, Workshops, Exhibitions and
+                Competitions.
+            </p>
+        </div>
+    );
+};
+export default About;
