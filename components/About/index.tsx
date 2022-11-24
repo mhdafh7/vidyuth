1787a5228f0c5dfa64343a71fef8b3f7f03c9eb0
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-25 00:17:55 +0530

    typo fixed

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -16,13 +16,13 @@ const About = () => {
                 />
             </div>
             <p>
-                Vidyuth is the annual technical fest of the Electrical and
-                Department, RIT Kottayam with a footfall of more than 20,000 and
-                an outreach to more than 50 colleges. It is a core campaign
-                unifying each EEEian to focus their gaze on progress, invention
-                and technology. It features an impressive line-up of sci-tech
-                events including Lectures, Workshops, Exhibitions and
-                Competitions.
+                <span>Vidyuth</span> is the annual technical fest of the
+                Electrical and Electronics Department, RIT Kottayam with a
+                footfall of more than 20,000 and an outreach to more than 50
+                colleges. It is a core campaign unifying each EEEian to focus
+                their gaze on progress, invention and technology. It features an
+                impressive line-up of sci-tech events including Lectures,
+                Workshops, Exhibitions and Competitions.
             </p>
         </div>
     );
