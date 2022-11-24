f38c7d584b5ddeefca31ded2382d3d9c0223b099
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-24 08:45:39 +0530

    feat: Footer basics

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,42 @@
-import styles from './Footer.module.scss'
+import styles from './Footer.module.scss';
+import { ImInstagram, ImFacebook2 } from 'react-icons/im';
+import Image from 'next/image';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    return (
+        <div className={styles.container}>
+            <div className={styles.socials}>
+                <h4>Follow us on social media</h4>
+                <ul>
+                    <li>
+                        <a
+                            href="https://www.instagram.com/vidhyut.techfest/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <ImInstagram size="2rem" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://m.facebook.com/profile.php?id=100087564614101"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <ImFacebook2 size="2rem" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.logo}>
+                <Image
+                    src="/footer-logo.png"
+                    alt="Vidyuth Logo"
+                    width={500}
+                    height={500}
+                />
+            </div>
+        </div>
+    );
+};
+export default Footer;
