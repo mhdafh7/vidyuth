2d223be522177e05827d2356e19640e7f98fdbb1
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-27 19:28:50 +0530

    feat: Added Events page

---
+++ pages/events/eventsList.ts
@@ -0,0 +1,118 @@
+// Day one events
+export const dayOneEvents = [
+  {
+    id: 1,
+    title: "ISRO Expo",
+    time: "9:00 am - 4:00 pm",
+    description: "Technical EXPO in collaboration with ISRO. Demonstrate ISRO's Space technology capabilities"
+  },
+  {
+    id: 2,
+    title: "OLA Electric Scooter Stall",
+    time: "9:00 am - 4:00 pm",
+    description: "Stall in collaboration with OLA Creates an opportunity for the students to explore advanced technologies in the EV sector."
+  },
+  {
+    id: 3,
+    title: "PROJECT expo",
+    time: "9:00 am - 4:00 pm",
+    description: "An opportunity for the students to experience the different project ideas."
+  },
+  {
+    id: 4,
+    title: "Gaming",
+    time: "9:00 am - 1:00 pm",
+    description: "The ultimate gaming experience for the students to get entertained by the leading LAN gaming platforms"
+  },
+  {
+    id: 5,
+    title: "3-D PRINTER demo",
+    time: "1:00 pm - 4:00 pm",
+    description: "Live demonstration of 3-D printing and their applications in various sectors."
+  },
+  {
+    id: 6,
+    title: "VR experience",
+    time: "9:00 am - 4:00 pm",
+    description: "A 360 degree viewing experience using Virtual Reality, giving students a chance to experience a world beyond their wildest imagination."
+  },
+  {
+    id: 7,
+    title: "Hackathon",
+    time: "9:00 am to 4:00 pm",
+    description: "Competitive event for finding solutions for current social issues by brainstorming sessions among the students teams."
+  },
+  {
+    id: 8,
+    title: "NEON Football",
+    time: "9:00 am to 4:00 pm",
+    description: "A thrillingly new experience for football lovers out there, for playing in an entirely different environment with your team."
+  },
+  {
+    id: 9,
+    title: "TECHNOHOOPS",
+    time: "9:00 am to 4:00 pm",
+    description: "By collaborating with basketball club 'HOOPERS RIT', we are taking basketball to the next level giving students, a hybrid experience of sport & technology."
+  },
+
+]
+
+// Day two events
+
+export const dayTwoEvents = [
+  {
+    id: 1,
+    title: "KSEB Expo",
+    time: "9:00 am - 4:00 pm",
+    description: "Technical EXPO in collaboration with KSEB,where the technical team is conducting an Expo for the students to familiarize with electrical equipments and working models to enhance the knowledge about power supply systems"
+  },
+  {
+    id: 2,
+    title: "IEEE Workshops",
+    time: "9:00 am - 4:00 pm",
+    description: "Technical Workshops in collaboration with IEEE SB RIT to nourish the technical knowledge of students."
+  },
+  {
+    id: 3,
+    title: "PROJECT Competition",
+    time: "9:00 am - 4:00 pm",
+    description: "Inter-college & Inter-school Project expo competition separately for both college and school students."
+  },
+  {
+    id: 4,
+    title: "Solar PV workshop",
+    time: "9:00 am - 12:00 pm",
+    description: "Introduction to solar photovoltaics and on-site experience of types of solar panel installations."
+  },
+  {
+    id: 5,
+    title: "ATHER e-scooter demonstration",
+    time: "9:00 am - 4:00 pm",
+    description: "The leading EV company - ATHER, showcasing their latest electric scooters and its internal components."
+  },
+  {
+    id: 6,
+    title: "Drone Building Workshop",
+    time: "9:00 am - 4:00 pm",
+    description: "Hands-on Experience on building a drone from scratch, and getting a deeper knowledge on UAVs and various simulation platforms for UAV design and control applications."
+  },
+  {
+    id: 7,
+    title: "IEDC workshops",
+    time: "9:00 am to 4:00 pm",
+    description: "Technical and non-technical workshops that rejuvenate the ideas of students on a startup journey."
+  },
+  {
+    id: 8,
+    title: "PCB Building workshop",
+    time: "9:00 am to 4:00 pm",
+    description: "Fundamentals and basic knowledge of a PCB design and construction is shared among the students in association with the event 'ATOM' of ISTE RIT."
+  },
+  {
+    id: 9,
+    title: "Fun & Minigames",
+    time: "9:00 am to 4:00 pm",
+    description: "Funfilled and entertaining minigames setup accross the department for making things more exciting for the students."
+  },
+
+]
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,5 +1,58 @@
 import styles from './events.module.scss';
+import { dayOneEvents, dayTwoEvents } from './eventsList';
+
 const Events = () => {
-    return <div className={styles.container}>Events</div>;
+    type EventProps = {
+        id: number;
+        title: string;
+        time: string;
+        description: string;
+    };
+    const Card = ({ id, title, time, description }: EventProps) => {
+        return (
+            <div
+                className={styles.card}
+                key={id}
+            >
+                <span className={styles.title}>{title}</span>
+                <span className={styles.time}>{time}</span>
+                <span className={styles.description}>{description}</span>
+            </div>
+        );
+    };
+
+    return (
+        <div className={styles.container}>
+            <h3>Events</h3>
+            <h4 className={styles.dayTitle}>Day One - 16th Dec</h4>
+            <div className={styles.cardsContainer}>
+                {dayOneEvents.map(({ id, title, time, description }) => {
+                    return (
+                        <Card
+                            key={id}
+                            id={id}
+                            title={title}
+                            time={time}
+                            description={description}
+                        />
+                    );
+                })}
+            </div>
+            <h4 className={styles.dayTitle}>Day Two - 17th Dec</h4>
+            <div className={styles.cardsContainer}>
+                {dayTwoEvents.map(({ id, title, time, description }) => {
+                    return (
+                        <Card
+                            key={id}
+                            id={id}
+                            title={title}
+                            time={time}
+                            description={description}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 export default Events;
