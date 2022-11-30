27c9a603dbe0bf98b20a5a4d618cd009650f7ae5
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-30 11:15:14 +0530

    Added head

--- pages/collaborators/index.tsx
+++ pages/collaborators/index.tsx
@@ -1,29 +1,43 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import { collaboratorsDetails } from '../../constants/constants';
 import styles from './collaborators.module.scss';
 
 const Collaborators = () => {
     return (
-        <div className={styles.container}>
-            <h3>Collaborators</h3>
-            <div className={styles.imgContainer}>
-                {collaboratorsDetails.map((item) => {
-                    return (
-                        <div
-                            className={styles.imgWrapper}
-                            key={item.id}
-                        >
-                            <Image
-                                src={item.img}
-                                fill
-                                alt={item.name}
-                                className={styles.img}
-                            />
-                        </div>
-                    );
-                })}
+        <>
+            <Head>
+                <title>Collaborators</title>
+                <meta
+                    name="description"
+                    content="Collaborators of Vidyuth 2022"
+                />
+                <link
+                    rel="icon"
+                    href="/favicon.ico"
+                />
+            </Head>
+            <div className={styles.container}>
+                <h3>Collaborators</h3>
+                <div className={styles.imgContainer}>
+                    {collaboratorsDetails.map((item) => {
+                        return (
+                            <div
+                                className={styles.imgWrapper}
+                                key={item.id}
+                            >
+                                <Image
+                                    src={item.img}
+                                    fill
+                                    alt={item.name}
+                                    className={styles.img}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 export default Collaborators;
