033597aca65e01cb9bcad45f0e1ea3155232b7e5
mhdafh7 <muhammed.aflah@leegality.com>
2022-12-01 16:58:29 +0530

    style: updated styles

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -10,7 +10,10 @@ const Navbar = () => {
     return (
         <>
             <nav className={styles.container}>
-                <Link href="/" className={styles.homeLogo}>
+                <Link
+                    href="/"
+                    className={styles.homeLogo}
+                >
                     <Image
                         src="/transparent-v.png"
                         alt="vidyuth logo"
@@ -89,7 +92,13 @@ const Navbar = () => {
                         className={styles.closeBtn}
                         onClick={handleClick}
                     >
-                        X
+                        <svg
+                            fill="#ffffff"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z" />
+                        </svg>
                     </button>
                     {/* </div> */}
                 </div>
